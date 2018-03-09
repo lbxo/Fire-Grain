134331dac982a7ace5ec5662e1c7cd4a1fb47403
Lucas Henderson <lucasbhenderson@me.com>
2018-03-08 19:55:57 -0500

    End of night - responsive fixes

--- js/app--topscroll.js
+++ js/app--topscroll.js
@@ -1,19 +1,19 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
     var offset = 120;
     var duration = 500;
      
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.top__scroll').fadeIn(duration);
-            } 
-        else {
-            jQuery('.top__scroll').fadeOut(duration);
-            }
-        });
-     
-    jQuery('.top__scroll').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-        })
+$(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+        $('.top__scroll').fadeIn(duration);
+        } 
+    else {
+        $('.top__scroll').fadeOut(duration);
+        }
     });
+    
+$('.top__scroll').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, duration);
+    return false;
+    })
+});
