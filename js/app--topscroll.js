edd9bdf7777d62800c935fe624b06ebbc23eaff8
Lucas Henderson <lucasbhenderson@me.com>
2018-03-15 15:13:12 -0400

    Making comments to html sheet

--- js/app--topscroll.js
+++ js/app--topscroll.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
     var offset = 120;
     var duration = 500;
      
-$(window).scroll(function() {
-    if ($(this).scrollTop() > offset) {
-        $('.top__scroll').fadeIn(duration);
-        } 
-    else {
-        $('.top__scroll').fadeOut(duration);
-        }
-    });
-    
-$('.top__scroll').click(function(event) {
-    event.preventDefault();
-    $('html, body').animate({scrollTop: 0}, duration);
-    return false;
-    })
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.top__scroll').fadeIn(duration);
+            } 
+        else {
+            $('.top__scroll').fadeOut(duration);
+            }
+        });
+        
+    $('.top__scroll').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+        });
 });
